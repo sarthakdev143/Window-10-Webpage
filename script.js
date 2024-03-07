f0ba39c9c1b835f7f25299fb3b1e99dc24711c57
Sarthak Parulekar <148764141+sarthakdev143@users.noreply.github.com>
2024-03-07 17:18:23 +0530

    Refactor CSS and JS for taskbar, notifications, and features. (generated by blackbox.ai )

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Search Bar --------------------------------------------------------------------------------------
+// Search Bar in The Taskbar -------------------------------------------------------------------------------------
 var searchBarDiv = document.querySelector(".searchBarDiv");
 var searchBar = document.querySelector(".searchBar");
 var searchIcon = document.querySelector(".searchIcon");
@@ -31,3 +31,60 @@ updateTime();
 timeSpan.innerHTML = timeSpan.textContent.replace(/^0+/, '');
 dateSpan.innerHTML = dateSpan.textContent.replace(/^0+/, '');
 
+// Notification Menu
+
+let notificationsIcon = document.querySelector(".notifications-icon");
+let notificationMenu = document.querySelector(".notification-menu");
+let noNoti = document.querySelector(".noNoti");
+let toggleMenu = false;
+
+notificationsIcon.addEventListener("click", function () {
+    if (toggleMenu == true) {
+        notificationMenu.style.marginRight = "-29%";
+        toggleMenu = false
+        noNoti.style.opacity = "0";
+    }
+
+    else {
+        notificationMenu.style.marginRight = "0";
+        toggleMenu = true;
+
+        setTimeout(function () {
+            noNoti.style.opacity = "1";
+        }, 500)
+    }
+})
+
+// Features Collapse And Expand
+let Collapse = document.querySelector(".collapse");
+let featuresParent = document.querySelector(".features-parent");
+let isCollapsed = false;
+
+Collapse.addEventListener("click", function () {
+    if (isCollapsed == false) {
+        featuresParent.style.marginTop = "21vw";
+        Collapse.innerHTML = "Expand";
+        isCollapsed = true;
+    }
+
+    else {
+        featuresParent.style.marginTop = "0";
+        Collapse.innerHTML = "Collapse";
+        isCollapsed = false;
+    }
+})
+
+// Feature / Box CLick
+let features = document.querySelectorAll(".box");
+let featureIcon = document.querySelectorAll(".feature-icon");
+let isOn = false;
+
+features.forEach(function (box) {
+    box.addEventListener("click", function () {
+        box.style.transform = "scale(0.95)";
+
+        setTimeout(function () {
+            box.style.transform = "scale(1)";
+        }, 200);
+    });
+});
